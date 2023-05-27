e029d5f3a133dbfa824f692f4261dd1558c9664c
kakl <martin.kakona@gmail.com>
2023-05-27 19:54:03 +0000

    initial version

---
+++ efm_down.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import time
+import serial
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+port = '/dev/ttyUSB1'
+
+baud = 9600
+
+ser = serial.Serial(port, baud, timeout=1)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+logname = "/home/ubuntu/data/vezicka/efm_down/efm_down.csv"
+handler = TimedRotatingFileHandler(logname, when='h', interval=1, utc=True)
+#handler.setLevel(logging.INFO)
+#handler.suffix = "%Y%m%d%H%M"
+logger.addHandler(handler)
+
+print(port, logname)
+
+while True:
+	reading = ser.readline()
+	if (len(reading) > 0):
+		data = str(int(round(time.time(),2))) + ',' + str(reading,'utf-8')
+		print(data)
+		logger.info(data)
+
+
