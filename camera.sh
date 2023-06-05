1012e1dec81527a43a5f70795f56eeaa61421660
kakl <martin.kakona@gmail.com>
2023-06-05 10:40:09 +0000

    date correction

--- camera.sh
+++ camera.sh
@@ -2,8 +2,11 @@
 
 while true
 do
+    NOW="$(date +"%Y-%m-%d_%R:%S")"
+    echo $NOW
     # write stream 1 hour long
-    ffmpeg -i http:/10.10.11.130:81/stream -t 1:00 -c mjpeg ~/data/vezicka/camera/"$(date + "%Y-%m-%d_%R:%S").mjpeg"
+    ffmpeg -i http:/10.10.11.130:81/stream -t 1:00 -c mjpeg ~/data/vezicka/camera/$NOW.mjpeg
     # delete files 5 days old
     find ~/data/vezicka/camera -mtime +1 -type f -delete
+    sleep 1
 done
