bd602b4eaf2b389f128db8bc98bb3e95b005e994
kakl <martin.kakona@gmail.com>
2023-06-05 10:57:07 +0000

    1h recording time, 5 days to delete

--- camera.sh
+++ camera.sh
@@ -5,8 +5,8 @@ do
     NOW="$(date +"%Y-%m-%d_%R:%S")"
     echo $NOW
     # write stream 1 hour long
-    ffmpeg -i http:/10.10.11.130:81/stream -t 1:00 -c mjpeg ~/data/vezicka/camera/$NOW.mjpeg
+    ffmpeg -i http:/10.10.11.130:81/stream -t 1:00:00 -c mjpeg ~/data/vezicka/camera/$NOW.mjpeg
     # delete files 5 days old
-    find ~/data/vezicka/camera -mtime +1 -type f -delete
+    find ~/data/vezicka/camera -mtime +5 -type f -delete
     sleep 1
 done
