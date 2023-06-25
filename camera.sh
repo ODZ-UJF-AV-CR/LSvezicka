0162da60b00755c5ea114bf99f8f77dbd052e0fe
kakl <martin.kakona@gmail.com>
2023-06-25 09:59:24 +0000

    36000 frames per file

--- camera.sh
+++ camera.sh
@@ -5,7 +5,7 @@ do
     NOW="$(date +"%Y-%m-%d_%R:%S")"
     echo $NOW
     # write stream 1 hour long
-    ffmpeg -use_wallclock_as_timestamps 1 -i http:/10.10.11.130:81/stream -t 3600 -c mjpeg ~/data/vezicka/camera/$NOW.mjpeg
+    ffmpeg -i http:/10.10.11.130:81/stream -frames 36000 -c mjpeg ~/data/vezicka/camera/$NOW.mjpeg
     # delete files 5 days old
     #!!!find ~/data/vezicka/camera -mtime +5 -type f -delete
     #sleep 1
