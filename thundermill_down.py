912def72ad2647b08c6f5eab78e8ddf3d4577c49
kakl <martin.kakona@gmail.com>
2024-12-01 19:07:32 +0000

    state up to december 2024

---
+++ thundermill_down.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import time
+import serial
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+time.sleep(30)
+
+port = '/dev/ttyUSB2'
+
+baud = 9600
+
+ser = serial.Serial(port, baud, timeout=10)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+logname = "/home/ubuntu/data/vezicka/thundermill_down/thundermill_down.csv"
+handler = TimedRotatingFileHandler(logname, when='h', interval=1, utc=True)
+#handler.setLevel(logging.INFO)
+#handler.suffix = "%Y%m%d%H%M"
+logger.addHandler(handler)
+
+print(port, logname)
+
+while True:
+	try:
+		reading = ser.readline()
+		if (len(reading) > 0):
+			data = str(round(time.time(),2)) + ',' + str(reading,'utf-8')
+			print(data)
+			logger.info(data)
+	except:
+		time.sleep(1)
+		continue
+
+
